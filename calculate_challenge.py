8ea931946c135f95d9eb2d849a2766f6f4c62afb
chenyufei <251813306@qq.com>
2019-10-14 15:16:20 +0800

    修改计算挑战字的方式，由调用C库的试修改直接使用第三方式的python代码

--- calculate_challenge.py
+++ calculate_challenge.py
@@ -1,13 +1,34 @@
-import ctypes
-def get_challenge(appeui, appkey, appnonce):
-    appeui = ctypes.c_char_p(appeui.encode('utf-8'))
-    appkey = ctypes.c_char_p(appkey.encode('utf-8'))
-    ll = ctypes.cdll.LoadLibrary
-    lib = ll("./libchallenge.so")
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+'''
+为了安装方便，可以直接使用下面的命令：
+pip install crypto pycryptodome
+pip uninstall crypto pycryptodome
+pip install pycryptodome
 
-    p = ctypes.create_string_buffer(32)
-    lib.get_challenge(appkey, appeui, appnonce, p)
-    challenge = p.raw
-    print(challenge)
-    return challenge.decode('utf-8')
+而如果你是linux环境，则直接安装pycryptodome即可：
+pip install pycryptodome
+'''
+import ctypes
+import sys
+from Cryptodome.Hash import CMAC
+from Cryptodome.Cipher import AES
+import binascii
+
+
+def get_challenge(appeui, appkey, appnonce):
+    secret = binascii.a2b_hex(appkey)
+    cobj = CMAC.new(secret, ciphermod=AES)
+    msg = binascii.a2b_hex(appeui) + \
+          binascii.a2b_hex(str('%08x' % appnonce)) + \
+          binascii.a2b_hex(str('%08x' % 0))
+    cobj.update(msg)
+    btr = cobj.digest()
+    challenge = ''
+    for x in range(0, 16):
+        challenge += ('%02X' % btr[16 - x - 1])
+    # print(challenge)
+    # challenge = cobj.hexdigest()
+    #print(challenge)
+    return challenge
 
