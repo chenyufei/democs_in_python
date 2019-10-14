d20ff5de484f8bcb99f857908d64be8306ff808d
chenyufei <251813306@qq.com>
2019-10-14 15:15:31 +0800

    删除以SO形式调用C代码的方式，直接使用python的方式生成挑战字

--- aes_cmac/challenge.h
+++
@@ -1,8 +0,0 @@
-#ifndef __CHALLENGE_H__
-#define __CHALLENGE_H__
-#include "claa_base.h"
-#define APPKEY_LEN (16)
-
-void challenge_identification(uint8 appkey[APPKEY_LEN], uint64 appeui, uint32 appnonce,char*channel);
-void get_challenge(char *appkey,char *appeui,uint32 appnonce, char *challenge);
-#endif
